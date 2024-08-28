783161dac207d6ef9814178a08520836db110cae
Teklemya <Teklemya@mail.uc.edu>
2024-08-28 03:15:11 -0400

    added fav icon, started the work page

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -1,9 +1,16 @@
 import "./MyWork.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
 
 const MyWork = () => {
   return (
-    <div>
-      
+    <div className="mywork">
+      <div className="mywork-title">
+        <h1>My latest Work</h1>
+        <img src={theme_pattern} alt="theme pattern" />
+      </div>
+      <div className="mywork-container">
+        
+      </div>
     </div>
   )
 }
