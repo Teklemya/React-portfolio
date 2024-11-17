5ad2c4a52843be97c4dfe687a9a4c16e59c367a8
Teklemya <Teklemya@mail.uc.edu>
2024-11-17 01:40:47 -0500

    eideted the nav p for light mode to white to make it visible

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import theme_pattern from "../../assets/theme_pattern.svg";
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import menu_open from '../../assets/menu_open.svg'
 import menu_close from '../../assets/menu_close.svg'
-
+ 
 const Navbar = () => {
   const menuRef = useRef();
 
