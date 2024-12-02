df7ac1d22a034e1844921d719dd3153f8334c835
Teklemya <Teklemya@mail.uc.edu>
2024-12-02 17:22:37 -0500

    updated footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <div className="footer-bottom-right">
             <p>Term of Services</p>
             <p>Privacy Policy</p>
-            <p>Connect with me</p>
+          
         </div>
 
       </div>
